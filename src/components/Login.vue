7f8d33798823bd2a896c0432ae23f2d75640ad1d
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-09-27 14:40:40 -0600

    Finished Login Screen

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,59 @@
 <template>
-    <div>
-
- <form action ="javascript:void(0)">
-        <h3>Email</h3>
-        <input type="text" id="email-input">
-        <h3>Password</h3>
-        <input type = "password" id="password-input">
-        <br>
-        <input type = "submit" value = "Login" id="login-submit">
-        </form>
-        
-<button @click="login" v-if="token == undefined">Login</button>
-
-    </div>
+  <div>
+    <p>Email</p>
+    <input type="text" id="" v-model="email" />
+    <p>Password</p>
+    <input type="password" id="" v-model="password" />
+    
+    <h3 @click="login" v-if="token == undefined"> Login </h3>
+    
+  </div>
 </template>
 
 <script>
-    export default {
-    name: "login-form",
 
+import axios from "axios";
+import cookies from "vue-cookies";
+
+export default {
+  name: "login-form",
+
+  data() {
+    return {
+      email: "",
+      password: "",
+      status: "",
+      token: cookies.get("loginToken"),
+    };
+  },
+
+  methods: {
+    login: function() {
+      axios
+        .request({
+          url: "https://reqres.in/api/login",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          data: {
+            email: this.email,
+            password: this.password
+          }
+        })
+        .then(response => {
+          console.log(response);
+           cookies.set("loginToken", response.data.token);
+           this.$router.push("/game")
+        })
+        .catch(error => {
+          console.log(error);
+          this.status = "Login Error!";
+        });
     }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,12 +1,10 @@
 <template>
     <div>
-  
+  <h2>Rock Paper Scissors</h2>
     </div>
 </template>
 
 <script>
-
-
 
     export default {
         name: "game-page",
